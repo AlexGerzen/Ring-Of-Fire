6d4e9f73d5702567d84270de06c4010fbc38a8d6
AlexGerzen <121639829+AlexGerzen@users.noreply.github.com>
2023-10-02 15:48:49 +0200

    Added drawing cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game!: Game;
+  currentCard: string = '';
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop(); // Last value from the Array will be taken and deleted from the Array
+      this.pickCardAnimation = true;
+      
+
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000)
+    }
+  }
 }
+
