bd96827a855b15149f1d65089a9e4695b19c9150
AlexGerzen <121639829+AlexGerzen@users.noreply.github.com>
2023-10-02 12:41:16 +0200

    Added stack of cards

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-startscreen',
   templateUrl: './startscreen.component.html',
   styleUrls: ['./startscreen.component.scss']
 })
-export class StartscreenComponent {
+export class StartscreenComponent implements OnInit {
+  
+  ngOnInit(): void {
+    
+  }
 
+  constructor(private router: Router) {
+
+  }
+
+  newGame() {
+    this.router.navigateByUrl('/game')
+  }
 }
