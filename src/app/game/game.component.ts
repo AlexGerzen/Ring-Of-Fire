204dda67a0daab0758006b64c2ee2b2bb11a96fb
AlexGerzen <121639829+AlexGerzen@users.noreply.github.com>
2023-10-03 19:22:05 +0200

    Some bug fixes

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -34,9 +34,10 @@ export class GameComponent implements OnInit {
     if (!this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop(); // Last value from the Array will be taken and deleted from the Array
       this.pickCardAnimation = true;
+
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
       
-
-
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
         this.pickCardAnimation = false;
@@ -48,7 +49,9 @@ export class GameComponent implements OnInit {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
 
     dialogRef.afterClosed().subscribe((name : string) => {
-      this.game.players.push(name);
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
     });
   }
 }
